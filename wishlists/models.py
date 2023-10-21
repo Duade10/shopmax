7bda0c23a956fbfc6bbe577653186bf91965bab7
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2023-10-21 23:35:47 +0100

    created wishlist model

--- wishlists/models.py
+++ wishlists/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from core.models import AbstractTimeStampModel
+
+
+class Wishlist(AbstractTimeStampModel):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="wishlist")
+    product = models.ManyToManyField("products.Product")
+
+    def __str__(self):
+        return f"{self.user} wishlist"
